0601ff8a08800d960a3d1c4ff4cb9a13f1684386
yudamhndra <mahendrayudapradana@mail.ugm.ac.id>
2023-09-20 18:03:12 +0700

    first commit

---
+++ app/src/main/java/com/example/tugaspertemuan5/HomepageActivity.kt
@@ -0,0 +1,58 @@
+package com.example.tugaspertemuan5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.tugaspertemuan5.RegisterActivity.Companion.EXTRA_USERNAME
+import com.example.tugaspertemuan5.databinding.ActivityHomepageBinding
+
+class HomepageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomepageBinding
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data = result.data
+
+                val username = data?.getStringExtra(EXTRA_USERNAME)
+                val email = data?.getStringExtra(RegisterActivity.EXTRA_EMAIL)
+                val phone = data?.getStringExtra(RegisterActivity.EXTRA_PHONE)
+
+                if (!username.isNullOrEmpty() && !email.isNullOrEmpty() && !phone.isNullOrEmpty()) {
+                    binding.txtUsername.text = "$username"
+                    binding.txtEmail.text = "$email"
+                    binding.txtPhone.text = "$phone"
+                }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomepageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val username = intent.getStringExtra(EXTRA_USERNAME)
+        val email = intent.getStringExtra(RegisterActivity.EXTRA_EMAIL)
+        val phone = intent.getStringExtra(RegisterActivity.EXTRA_PHONE)
+
+        with(binding){
+
+            binding.txtUsername.text = "$username"
+            binding.txtEmail.text = "$email"
+            binding.txtPhone.text = "$phone"
+
+            val btnLogout = findViewById<Button>(R.id.btn_logout)
+            btnLogout.setOnClickListener {
+                val intentToRegisterActivity = Intent(this@HomepageActivity, RegisterActivity::class.java)
+
+                intentToRegisterActivity.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+                startActivity(intentToRegisterActivity)
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/tugaspertemuan5/RegisterActivity.kt
@@ -0,0 +1,48 @@
+package com.example.tugaspertemuan5
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.tugaspertemuan5.databinding.ActivityRegisterBinding
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
+    companion object {
+        const val EXTRA_USERNAME = "username"
+        const val EXTRA_EMAIL = "email"
+        const val EXTRA_PHONE = "phone"
+        const val EXTRA_PASSWORD = "password"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+
+            btnRegister.setOnClickListener {
+
+                val intentToHome = Intent(this@RegisterActivity, HomepageActivity::class.java)
+
+                val username = edtUsername.text.toString()
+                val email = edtEmail.text.toString()
+                val phone = edtPhonenum.text.toString()
+                val password = edtPassword.text.toString()
+
+                intentToHome.putExtra(EXTRA_USERNAME, username)
+                intentToHome.putExtra(EXTRA_EMAIL, email)
+                intentToHome.putExtra(EXTRA_PHONE, phone)
+                intentToHome.putExtra(EXTRA_PASSWORD, password)
+
+                startActivity(intentToHome)
+
+            }
+        }
+
+    }
+}
